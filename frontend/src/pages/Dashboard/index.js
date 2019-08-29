2c1dde1a92500003c1814f270b6d2798852a7958
Henrique Augusto <hick_97@hotmail.com>
2019-08-29 13:37:26 -0300

    dashboard screen

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, ContentHeader, Meetup } from './styles';
 
 export default function Dashboard() {
-  api.get('meetups');
-  return <h1>Dashboard</h1>;
+  const [meetups, setMeetups] = useState([]);
+  useEffect(() => {
+    async function loadMeetups() {
+      const response = await api.get('/user/meetups');
+
+      const formattedResponse = response.data.map(meetup => {
+        return {
+          id: meetup.id,
+          title: meetup.title,
+          date: format(parseISO(meetup.date), "d 'de' MMMM ', às ' HH'h'", {
+            locale: pt,
+          }),
+        };
+      });
+
+      setMeetups(formattedResponse);
+    }
+    loadMeetups();
+  }, []);
+  return (
+    <Container>
+      <ContentHeader>
+        <h1>Meus meetups</h1>
+        <Link to="/meetup">
+          <button>Novo meetup</button>
+        </Link>
+      </ContentHeader>
+
+      <ul>
+        {meetups.map(meetup => (
+          <Link to={`/meetup/${meetup.id}/details`}>
+            <Meetup>
+              <strong>{meetup.title}</strong>
+              <span>{meetup.date}</span>
+            </Meetup>
+          </Link>
+        ))}
+      </ul>
+    </Container>
+  );
 }
